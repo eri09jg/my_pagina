46a521f56c3c97f5be9eaa7e93caab71733c3c84
eri09jg <garciaeri567@gmail.com>
2025-04-22 15:10:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const navToggle = document.getElementById("nav-toggle");
     const navMenu = document.querySelector(".nav-menu");
 
-    navToggle.addEventListener("click", function() {
-        navMenu.classList.toggle("nav-menu_visible");
+    navToggle.addEventListener("click", function () {
+        const isVisible = navMenu.classList.toggle("nav-menu_visible");
+
+        if (isVisible) {
+            document.body.classList.add("no-scroll");
+        } else {
+            document.body.classList.remove("no-scroll");
+        }
     });
 });
